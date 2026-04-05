92632016b29881fbc5c9188ffabebf89f5abae02
myeonghwi <automaster5013@gmail.com>
2026-04-05 12:20:25 +0900

    파이썬_코딩_도구

---
+++ 01/002.py
@@ -0,0 +1,6 @@
+hello = '안녕하세요 파이썬!'
+print(hello)
+a = 1
+b = 10
+print(a+b)
+
---
+++ 01/003.py
@@ -0,0 +1,14 @@
+hello = '안녕하세요 파이썬!'
+print(hello)
+a = 1
+b = 10
+print(a+b)
+
+hello2 = '안녕하세요 독자님!'
+print(hello2)
+
+pi = 3.141592
+e = 2.71
+print(pi+e)
+
+
