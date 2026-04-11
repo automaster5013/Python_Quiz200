16c908eca5234e1bf9ec526cefb6e4a0df3320fc
myeonghwi <automaster5013@gmail.com>
2026-04-11 21:36:39 +0900

    초보자를 위한 python_200제(2판)

---
+++ 03_제어문/023_비교연산자.py
@@ -0,0 +1,13 @@
+x = 1; y = 2
+strData1 = 'Hello'; strData2 = 'Python'
+print(x == y)
+print(x != y)
+print(strData1 == strData2)
+print(strData2 == 'Python')
+print(strData1 < strData2)
+
+
+
+
+
+
