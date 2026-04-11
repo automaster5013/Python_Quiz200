0bfd714baf58e4fd838c8665ab01b7e30bcb6c0f
myeonghwi <automaster5013@gmail.com>
2026-04-11 22:19:32 +0900

    초보자를 위한 python_200제(2판)

---
+++ 03_제어문/024_논리연산자_and_or_not.py
@@ -0,0 +1,16 @@
+x = 1; y = 2
+strData1 = 'Hello'; strData2 = 'Python'
+flag1 = x == y                  # False
+flag2 = x < y                   # True
+flag3 = strData1 != strData2    # True
+flag4 = strData1 > strData2     # False
+
+print(flag1 and flag2)
+print(flag2 and flag3)
+print(flag3 or flag4)
+print(flag1 or flag4)
+print(not flag1)
+print(not flag2)
+
+
+
